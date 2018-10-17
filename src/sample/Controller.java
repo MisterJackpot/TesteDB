5e160b0ab1a76c51859d0c0f40fff5d2dfe5f0c8
Gabriel Loff Sa De Moraes <gabriel.moraes@poa01.local>
2018-10-17 12:15:08 -0300

    Populating Choice Boxes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,52 @@
 package sample;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 
 public class Controller {
+    @FXML
+    private URL location;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private ChoiceBox Profissional;
+
+    @FXML
+    private ChoiceBox Restaurante;
+
+    public Controller()
+    {
+    }
+
+    @FXML
+    private void initialize()
+    {
+        ArrayList profissionais = new ArrayList();
+        profissionais.add("Juca");
+        profissionais.add("João");
+        profissionais.add("Luisa");
+        profissionais.add("Ariel");
+        Profissional.getItems().setAll(profissionais);
+
+        ArrayList restaurantes = new ArrayList();
+        restaurantes.add("Palatus");
+        restaurantes.add("Predio 32");
+        restaurantes.add("Novo Sabor");
+        restaurantes.add("Subway");
+        restaurantes.add("Madero Container");
+        restaurantes.add("Severo Garage");
+        Restaurante.getItems().setAll(restaurantes);
+    }
+
+
 
 
 }
