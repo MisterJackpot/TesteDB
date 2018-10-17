93a12e814851f5c6b776ca45f04cf40a15be5295
Gabriel Loff <gabriel.loff@acad.pucrs.br>
2018-10-17 16:16:16 -0300

    Logic of the voting and some validation

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,6 +8,8 @@ import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 
@@ -27,21 +29,31 @@ public class Controller {
     @FXML
     private Label Resultado;
 
+    private ArrayList profissionais;
+    private ArrayList restaurantes;
+    private ArrayList profissionaisAux;
+    private ArrayList restaurantesAux;
+    private HashMap<String,Integer> votos;
+
+
     public Controller()
     {
+        restaurantes = new ArrayList();
+        profissionais = new ArrayList();
+        profissionaisAux = new ArrayList();
+        restaurantesAux = new ArrayList();
+        votos = new HashMap<>();
     }
 
     @FXML
     private void initialize()
     {
-        ArrayList profissionais = new ArrayList();
         profissionais.add("Juca");
         profissionais.add("João");
         profissionais.add("Luisa");
         profissionais.add("Ariel");
         Profissional.getItems().setAll(profissionais);
 
-        ArrayList restaurantes = new ArrayList();
         restaurantes.add("Palatus");
         restaurantes.add("Predio 32");
         restaurantes.add("Novo Sabor");
@@ -49,11 +61,26 @@ public class Controller {
         restaurantes.add("Madero Container");
         restaurantes.add("Severo Garage");
         Restaurante.getItems().setAll(restaurantes);
+
+        profissionaisAux.addAll(profissionais);
+        restaurantesAux.addAll(restaurantes);
+
+
     }
 
     @FXML
     private void vote(){
-        Resultado.setText("Voto Computado");
+        if(profissionaisAux.contains(Profissional.getValue())) {
+            Resultado.setText("Voto Computado " + Profissional.getValue());
+            if(votos.containsKey(Restaurante.getValue())){
+                votos.replace(Restaurante.getValue().toString(),votos.get(Restaurante.getValue())+1);
+            }else{
+                votos.put(Restaurante.getValue().toString(),1);
+            }
+            profissionaisAux.remove(Profissional.getValue());
+        }else{
+            Resultado.setText("Profissional já votou nesta data");
+        }
         Timeline timeline = new Timeline(new KeyFrame(
                 Duration.millis(2500),
                 ae -> changeText()));
@@ -61,7 +88,20 @@ public class Controller {
     }
 
     private void changeText(){
-        if(Resultado.getText() != "Aguardando Conclusão") Resultado.setText("Aguardando Conclusão");
+        if(profissionaisAux.isEmpty()){
+            String resultado = "";
+            int aux = 0;
+            for (Map.Entry<String, Integer> voto : votos.entrySet()) {
+                if(voto.getValue() > aux){
+                    resultado = voto.getKey();
+                    aux = voto.getValue();
+                }
+            }
+            Resultado.setText("Almoço hoje: " + resultado);
+            restaurantesAux.remove(resultado);
+        }else {
+            if (Resultado.getText() != "Aguardando Conclusão") Resultado.setText("Aguardando Conclusão");
+        }
     }
 
 
